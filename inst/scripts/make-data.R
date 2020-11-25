b8cf3892d069b2cfd35cd1ca78236e4b65977c27
MarvinQuiet <mawenjing1993@126.com>
2020-11-25 11:10:22 -0500

    add description on how to make the data

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,3 +1,4 @@
-'''
-
-'''
+# The mouse brain data was originally downloaded from GSE116470: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE116470
+# The human brain data was originally downloaded from GSE144136: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE144136
+# According to the cell type annotation of the data, we applied an algorithm (Marques et al. 2016)
+# on the read count matrix and generate a cell-type specific gene enrichment scores.
