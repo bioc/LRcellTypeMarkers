c4853c1c6cf18c5d2dbc906bbd755d2996deb35d
MarvinQuiet <mawenjing1993@126.com>
2020-12-06 19:58:11 -0500

    add details on how to make the data

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,6 +1,24 @@
-# The mouse brain data was originally downloaded from http://dropviz.org/, a data resource provided by the author
-# The human brain data was originally downloaded from GSE144136: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE144136
-# According to the cell type annotation of the data, we applied an algorithm (Marques et al. 2016)
-# on the read count matrix and generate a cell-type specific gene enrichment scores.
+# --- The gene enrichment algorithm
+# We obey the clustering result from the original papers and applied an
+# algorithm (Marques et al. 2016) on the processed read count matrix.
+# In the end, cell-type specific gene enrichment scores are calculated.
 
+# --- The mouse brain data
+# 1. Read count matrix from 9 brain region was downloaded from
+#    http://dropviz.org/ under Data tab.
+# 2. Cell annotation was downloaded from the same URL with subcluster information.
+# 3. Only cells with annotation are remained in the read count matrix.
+# 4. Common genes (15,976) among all 9 brain regions are remained in the read
+#    count matrix.
+# 5. Enriched gene scores are then calculated using the processed read count matrix
+#    for each brain region.
 
+# --- The human brain data
+# 1. Read count matrix was downloaded from GSE144136:
+#    https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE144136
+# 2. Cell annotation was also downloaded from the same URL. 'Astros_1', 'Mix_1',
+#    'Mix_2', 'Mix_3', 'Mix_4', 'Mix_5', 'Inhib_4_SST' were excluded according
+#    to paper.
+# 3. Genes expressed in less than 10 cells are filtered; Cells having less
+#    than 10 read counts are filtered
+# 4. Enriched gene scores are then calculated using the processed read count matrix.
